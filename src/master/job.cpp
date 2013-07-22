38ded7c05efca5081003b24b435332872003fcec
budnik <budnik@ok.by>
2013-07-22 17:31:36 +0300

    Job, JobQueue class initial implementation

--- src/master/job.cpp
+++ src/master/job.cpp
@@ -1,1 +1,33 @@
 #include "job.h"
+
+namespace master {
+
+void JobQueue::PushJob( Job *job )
+{
+    boost::mutex::scoped_lock scoped_lock( jobsMut_ );
+    jobs_.push_back( job );
+    ++numJobs_;
+}
+
+Job *JobQueue::PopJob()
+{
+    boost::mutex::scoped_lock scoped_lock( jobsMut_ );
+    if ( numJobs_ )
+    {
+        Job *j = jobs_.front();
+        jobs_.pop_front();
+        --numJobs_;
+        return j;
+    }
+    return NULL;
+}
+
+Job *JobQueue::GetTopJob()
+{
+    boost::mutex::scoped_lock scoped_lock( jobsMut_ );
+    if ( numJobs_ )
+        return jobs_.front();
+    return NULL;
+}
+
+} // namespace master
--- src/master/job.h
+++ src/master/job.h
@@ -1,15 +1,44 @@
+#include <list>
+#include <boost/thread/mutex.hpp>
+
 namespace master {
 
 class Job
 {
 public:
+    Job( const char *script, const char *scriptLanguage, unsigned int maxNodes, int timeout )
+    : script_( script ), scriptLanguage_( scriptLanguage ), maxNodes_( maxNodes ), timeout_( timeout )
+    {
+        scriptLength_ = script_.size();
+    }
+
+	unsigned int GetScriptLength() const { return scriptLength_; }
+	const std::string &GetScriptLanguage() const { return scriptLanguage_; }
+	const std::string &GetScript() const { return script_; }
+    unsigned int GetMaxNodes() const { return maxNodes_; }
+    int GetTimeout() const { return timeout_; }
+
 private:
+    std::string script_;
+    std::string scriptLanguage_;
+    unsigned int scriptLength_;
+    unsigned int maxNodes_;
+    int timeout_;
 };
 
 class JobQueue
 {
 public:
+    JobQueue() : numJobs_( 0 ) {}
+
+    void PushJob( Job *job );
+    Job *PopJob();
+    Job *GetTopJob();
+
 private:
+    std::list< Job * > jobs_;
+    int numJobs_;
+    boost::mutex jobsMut_;
 };
 
 } // namespace master
