84841abbe2fc32af5c0d1643b4f3193089fc62f7
budnik <budnik@ok.by>
2013-09-05 12:59:26 +0300

    java support

--- node/node.java
+++ node/node.java
@@ -1,77 +1,219 @@
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.lang.reflect.InvocationTargetException;
+import java.io.FileOutputStream;
 import java.net.URI;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.tools.Diagnostic;
-import javax.tools.DiagnosticCollector;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+
 import javax.tools.JavaCompiler;
 import javax.tools.JavaFileObject;
 import javax.tools.SimpleJavaFileObject;
 import javax.tools.ToolProvider;
 import javax.tools.JavaCompiler.CompilationTask;
 import javax.tools.JavaFileObject.Kind;
+import javax.tools.ForwardingJavaFileManager;
+
+import java.security.SecureClassLoader;
+import javax.tools.JavaFileManager;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.FileObject;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Method;
+
 
 public class node {
-  public static void main(String args[]) throws IOException {
-    JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-    DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
+    public static void main(String args[]) {
+        int errCode = 0;
+        try {
+            String shmemPath = args[1];
+            long scriptLen = Long.valueOf( args[2] ).longValue();
+            long shmemOffset = Long.valueOf( args[3] ).longValue();
 
-    StringWriter writer = new StringWriter();
-    PrintWriter out = new PrintWriter(writer);
-    out.println("public class HelloWorld {");
-    out.println("  public static void main(String args[]) {");
-    out.println("    System.out.println(\"This is in another java file\");");    
-    out.println("  }");
-    out.println("}");
-    out.close();
-    JavaFileObject file = new JavaSourceFromString("HelloWorld", writer.toString());
+            RandomAccessFile shmem = new RandomAccessFile( shmemPath, "r" );
+            MappedByteBuffer buf = shmem.getChannel().map( FileChannel.MapMode.READ_ONLY, shmemOffset, scriptLen );
 
-    Iterable<? extends JavaFileObject> compilationUnits = Arrays.asList(file);
-    CompilationTask task = compiler.getTask(null, null, diagnostics, null, null, compilationUnits);
-    
-    boolean success = task.call();
-    for (Diagnostic diagnostic : diagnostics.getDiagnostics()) {
-      System.out.println(diagnostic.getCode());
-      System.out.println(diagnostic.getKind());
-      System.out.println(diagnostic.getPosition());
-      System.out.println(diagnostic.getStartPosition());
-      System.out.println(diagnostic.getEndPosition());
-      System.out.println(diagnostic.getSource());
-      System.out.println(diagnostic.getMessage(null));
+            byte[] bytes = new byte[ (int)scriptLen ];
+            buf.get( bytes );
+            String s = new String( bytes, "UTF-8" );
 
+            String className = "Main";
+
+            JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+
+            JavaFileManager fileManager = new ClassFileManager( compiler.getStandardFileManager( null, null, null ) );
+
+            ArrayList<JavaFileObject> jfiles = new ArrayList<JavaFileObject>();
+            jfiles.add( new CharSequenceJavaFileObject( className, s ) );
+
+            CompilationTask task = compiler.getTask( null, fileManager, null, null, null, jfiles );
+            if ( task.call() ) {
+                Class aClass = fileManager.getClassLoader( null ).loadClass( className );
+                Object obj = aClass.newInstance();
+                Method method = aClass.getDeclaredMethod( "main", new Class[] { String[].class } );
+                String arglist[] = new String[2];
+                arglist[0] = args[4];
+                arglist[1] = args[5];
+                method.invoke( obj, (Object)arglist );
+            }
+            else {
+                errCode = -1;
+            }
+        }
+        catch( Exception e ) {
+            errCode = -1;
+            System.err.println( e );
+        }
+
+        try {
+            String fifoName = args[0];
+            FileOutputStream fifo = new FileOutputStream( fifoName );
+            fifo.write( intToByteArray( errCode ) );
+            fifo.close();
+        }
+        catch( Exception e ) {
+            System.err.println( e );
+        }
     }
-    System.out.println("Success: " + success);
 
-    if (success) {
-      try {
-        Class.forName("HelloWorld").getDeclaredMethod("main", new Class[] { String[].class })
-            .invoke(null, new Object[] { null });
-      } catch (ClassNotFoundException e) {
-        System.err.println("Class not found: " + e);
-      } catch (NoSuchMethodException e) {
-        System.err.println("No such method: " + e);
-      } catch (IllegalAccessException e) {
-        System.err.println("Illegal access: " + e);
-      } catch (InvocationTargetException e) {
-        System.err.println("Invocation target: " + e);
-      }
+    public static final byte[] intToByteArray(int value) {
+        return new byte[] {
+            (byte)(value >>> 24),
+            (byte)(value >>> 16),
+            (byte)(value >>> 8),
+            (byte)value};
     }
-  }
 }
 
-class JavaSourceFromString extends SimpleJavaFileObject {
-  final String code;
+class CharSequenceJavaFileObject extends SimpleJavaFileObject {
+    /**
+    * CharSequence representing the source code to be compiled
+    */
+    private CharSequence content;
 
-  JavaSourceFromString(String name, String code) {
-    super(URI.create("string:///" + name.replace('.','/') + Kind.SOURCE.extension),Kind.SOURCE);
-    this.code = code;
-  }
+    /**
+    * This constructor will store the source code in the
+    * internal "content" variable and register it as a
+    * source code, using a URI containing the class full name
+    *
+    * @param className
+    *            name of the public class in the source code
+    * @param content
+    *            source code to compile
+    */
+    public CharSequenceJavaFileObject(String className,
+        CharSequence content) {
+        super(URI.create("string:///" + className.replace('.', '/')
+            + Kind.SOURCE.extension), Kind.SOURCE);
+        this.content = content;
+    }
 
-  @Override
-  public CharSequence getCharContent(boolean ignoreEncodingErrors) {
-    return code;
-  }
+    /**
+    * Answers the CharSequence to be compiled. It will give
+    * the source code stored in variable "content"
+    */
+    @Override
+    public CharSequence getCharContent(
+        boolean ignoreEncodingErrors) {
+        return content;
+    }
+}
+
+class JavaClassObject extends SimpleJavaFileObject {
+    /**
+    * Byte code created by the compiler will be stored in this
+    * ByteArrayOutputStream so that we can later get the
+    * byte array out of it
+    * and put it in the memory as an instance of our class.
+    */
+    protected final ByteArrayOutputStream bos =
+        new ByteArrayOutputStream();
+
+    /**
+    * Registers the compiled class object under URI
+    * containing the class full name
+    *
+    * @param name
+    *            Full name of the compiled class
+    * @param kind
+    *            Kind of the data. It will be CLASS in our case
+    */
+    public JavaClassObject(String name, Kind kind) {
+        super(URI.create("string:///" + name.replace('.', '/')
+            + kind.extension), kind);
+    }
+
+    /**
+    * Will be used by our file manager to get the byte code that
+    * can be put into memory to instantiate our class
+    *
+    * @return compiled byte code
+    */
+    public byte[] getBytes() {
+        return bos.toByteArray();
+    }
+
+    /**
+    * Will provide the compiler with an output stream that leads
+    * to our byte array. This way the compiler will write everything
+    * into the byte array that we will instantiate later
+    */
+    @Override
+    public OutputStream openOutputStream() throws IOException {
+        return bos;
+    }
+}
+
+class ClassFileManager extends ForwardingJavaFileManager {
+    /**
+    * Instance of JavaClassObject that will store the
+    * compiled bytecode of our class
+    */
+    private JavaClassObject jclassObject;
+
+    /**
+    * Will initialize the manager with the specified
+    * standard java file manager
+    *
+    * @param standardManger
+    */
+    public ClassFileManager(StandardJavaFileManager
+        standardManager) {
+        super(standardManager);
+    }
+
+    /**
+    * Will be used by us to get the class loader for our
+    * compiled class. It creates an anonymous class
+    * extending the SecureClassLoader which uses the
+    * byte code created by the compiler and stored in
+    * the JavaClassObject, and returns the Class for it
+    */
+    @Override
+    public ClassLoader getClassLoader(Location location) {
+        return new SecureClassLoader() {
+            @Override
+            protected Class<?> findClass(String name)
+                throws ClassNotFoundException {
+                byte[] b = jclassObject.getBytes();
+                return super.defineClass(name, jclassObject
+                    .getBytes(), 0, b.length);
+            }
+        };
+    }
+
+    /**
+    * Gives the compiler an instance of the JavaClassObject
+    * so that the compiler can write the byte code into it.
+    */
+    @Override
+    public JavaFileObject getJavaFileForOutput(Location location,
+        String className, Kind kind, FileObject sibling)
+            throws IOException {
+            jclassObject = new JavaClassObject(className, kind);
+        return jclassObject;
+    }
 }
---
+++ test/sendscript/hello.java
@@ -0,0 +1,5 @@
+public class Main {
+  public static void main(String args[]) {
+      System.out.println("Java started: taskId="+args[0]+", numTasks="+args[1]);
+  }
+}
