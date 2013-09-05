c1df40491aee481b36eb01394a55ae32deab231d
abudnik <budnik27@gmail.com>
2013-09-05 22:37:49 +0300

    further impl.

--- node/node.rb
+++ node/node.rb
@@ -6,6 +6,22 @@ begin
   shmemOffset = Integer(ARGV[3])
   taskId = Integer(ARGV[4])
   numTasks = Integer(ARGV[5])
+
+  file = File.open(shmemPath, "r")
+  file.seek(shmemOffset, IO::SEEK_SET)
+  buffer = file.read(scriptLen)
+
+  errCode = eval(buffer)
+rescue StandardError => e
+  print e
+  errCode = -1
+end
+
+begin
+  fifoName = ARGV[0]
+  fifo = File.open(fifoName, "w")
+  fifo.write( errCode )
+  fifo.close()
 rescue StandardError => e
   print e
 end
