8e97406d6e64aef1bb1d5fc66b0d9d17ea07c24c
budnik <budnik@ok.by>
2013-09-06 10:50:28 +0300

    ruby support

--- node/node.rb
+++ node/node.rb
@@ -4,24 +4,29 @@ begin
   shmemPath = ARGV[1]
   scriptLen = Integer(ARGV[2])
   shmemOffset = Integer(ARGV[3])
-  taskId = Integer(ARGV[4])
-  numTasks = Integer(ARGV[5])
+  taskId = ARGV[4]
+  numTasks = ARGV[5]
 
-  file = File.open(shmemPath, "r")
+  file = File.open(shmemPath, 'r')
   file.seek(shmemOffset, IO::SEEK_SET)
   buffer = file.read(scriptLen)
 
-  errCode = eval(buffer)
-rescue StandardError => e
-  print e
+  inject = "taskId=" + taskId + "\n"
+  inject += "numTasks=" + numTasks + "\n"
+
+  eval(inject+buffer)
+rescue Exception => e
   errCode = -1
+  puts e.message
+  puts e.backtrace.inspect
 end
 
 begin
   fifoName = ARGV[0]
-  fifo = File.open(fifoName, "w")
-  fifo.write( errCode )
+  fifo = File.open(fifoName, 'w')
+  fifo.write( [errCode].pack('V') )
   fifo.close()
-rescue StandardError => e
-  print e
+rescue Exception => e
+  puts e.message
+  puts e.backtrace.inspect
 end
--- test/example/hello.rb
+++ test/example/hello.rb
@@ -1,1 +1,2 @@
 puts 'Hello, Ruby!'
+puts "taskId="+taskId.to_s()+", numTasks="+numTasks.to_s()
