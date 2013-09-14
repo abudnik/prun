cea42e6e575d82c44503651efc118548116b3e6b
abudnik <budnik27@gmail.com>
2013-09-14 14:47:52 +0300

    small fix

--- test/example/hello.py
+++ test/example/hello.py
@@ -1,9 +1,2 @@
-import os
-
-print "Hello, PyServer!"
-#os.unlink("/home/Andrew/Project/ff")
-#try:
-##	sys.exit()
-#	os._exit(1)
-#except SystemExit:
-#	pass
+print( "Hello, Python!" )
+print( "taskId:%d, numTasks:%d" % (taskId, numTasks) )
