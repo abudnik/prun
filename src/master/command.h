5f607559c47a3cf0077b134220950993d80e73fb
abudnik <budnik27@gmail.com>
2013-10-28 16:30:29 +0300

    command sender initial commit

---
+++ src/master/command.h
@@ -0,0 +1,42 @@
+#ifndef __COMMAND_H
+#define __COMMAND_H
+
+#include <sstream>
+#include <boost/shared_ptr.hpp>
+
+namespace master {
+
+class Command
+{
+public:
+    typedef std::multimap<std::string, std::string> Params;
+    typedef std::pair< std::string, std::string > PairType;
+
+public:
+    virtual ~Command() {}
+
+    void SetParam( const std::string &key, const std::string &value )
+    {
+        params_.insert( PairType( key, value ) );
+    }
+
+    template< typename T >
+    void SetParam( const std::string &key, const T &value )
+    {
+        std::ostringstream ss;
+        ss << value;
+        params_.insert( PairType( key, ss.str() ) );
+    }
+
+    Params &GetAllParams() { return params_; }
+
+protected:
+    std::string command_;
+    Params params_;
+};
+
+typedef boost::shared_ptr< Command > CommandPtr;
+
+} // namespace master
+
+#endif
