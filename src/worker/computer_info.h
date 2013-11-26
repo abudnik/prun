ade947fa00946fea67e3d0e7a1d1466a4ea7bd4c
abudnik <budnik27@gmail.com>
2013-11-26 21:50:45 +0300

    memory size param added

--- src/worker/computer_info.h
+++ src/worker/computer_info.h
@@ -1,6 +1,7 @@
 #ifndef __COMPUTER_INFO_H
 #define __COMPUTER_INFO_H
 
+#include <unistd.h>
 #include <boost/thread.hpp>
 
 namespace worker {
@@ -10,10 +11,16 @@ class ComputerInfo
     ComputerInfo()
     {
         numCPU_ = boost::thread::hardware_concurrency();
+
+        long pages = sysconf( _SC_PHYS_PAGES );
+        long page_size = sysconf( _SC_PAGE_SIZE );
+        memSize_ = pages * page_size;
     }
 
 public:
     int GetNumCPU() const { return numCPU_; }
+
+    int64_t GetPhysicalMemory() const { return memSize_; }
 
     static ComputerInfo &Instance()
     {
@@ -23,6 +30,7 @@ public:
 
 private:
     int numCPU_;
+    int64_t memSize_;
 };
 
 } // namespace worker
