d66fdf5fc404efb2d03e51d6290434bbc527e9bc
abudnik <budnik27@gmail.com>
2014-04-16 11:16:13 +0300

    more tests

--- src/test/unit.cpp
+++ src/test/unit.cpp
@@ -3,6 +3,8 @@
 #include <boost/test/unit_test.hpp>
 #include <vector>
 #include "master/worker_manager.h"
+#include "master/scheduler.h"
+#include "common/service_locator.h"
 
 using namespace std;
 using namespace master;
@@ -26,6 +28,73 @@ BOOST_AUTO_TEST_CASE( host_uniqueness )
     vector< WorkerPtr > workersVector;
     GetWorkers( workersVector, "grp2" );
     BOOST_CHECK_EQUAL( workersVector.size(), 1 );
+}
+
+BOOST_AUTO_TEST_CASE( group_removal )
+{
+    for( int i = 0; i < 5; ++i )
+    {
+        AddWorkerHost( "grp1", "host1" );
+        AddWorkerHost( "grp2", "host2" );
+        AddWorkerHost( "grp3", "host3" );
+    }
+
+    DeleteWorkerGroup( "grp2" );
+    vector< WorkerPtr > workersVector;
+    GetWorkers( workersVector, "grp2" );
+    BOOST_CHECK( workersVector.empty() );
+}
+
+BOOST_AUTO_TEST_CASE( host_removal )
+{
+    for( int i = 0; i < 5; ++i )
+    {
+        AddWorkerHost( "grp2", "host1" );
+        AddWorkerHost( "grp2", "host2" );
+        AddWorkerHost( "grp2", "host3" );
+    }
+
+    DeleteWorkerHost( "host2" );
+    vector< WorkerPtr > workersVector;
+    GetWorkers( workersVector, "grp2" );
+    BOOST_CHECK_EQUAL( workersVector.size(), 2 );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
+struct WorkerManagerEnvironment
+{
+    WorkerManagerEnvironment()
+    {
+        common::ServiceLocator &serviceLocator = common::ServiceLocator::Instance();
+        serviceLocator.Register( (master::IScheduler*)&sched );
+    }
+
+    ~WorkerManagerEnvironment()
+    {
+        common::ServiceLocator::Instance().UnregisterAll();
+    }
+
+    WorkerManager mgr;
+    Scheduler sched;
+};
+
+BOOST_FIXTURE_TEST_SUITE( WorkerManagerIntegrationSuite, WorkerManagerEnvironment )
+
+BOOST_AUTO_TEST_CASE( host_ping_response )
+{
+    mgr.AddWorkerHost( "grp", "host1" );
+
+    std::vector< WorkerPtr > workers;
+    mgr.GetWorkers( workers );
+
+    BOOST_REQUIRE_EQUAL( workers.size(), 1 );
+
+    BOOST_CHECK_EQUAL( mgr.GetTotalWorkers(), 0 );
+    mgr.SetWorkerIP( workers[0], "127.0.0.1" );
+    mgr.OnNodePingResponse( "127.0.0.1", 2, 1024 );
+    BOOST_CHECK_EQUAL( mgr.GetTotalWorkers(), 1 );
 }
 
 BOOST_AUTO_TEST_SUITE_END()
